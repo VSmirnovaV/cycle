8849c55504921b26b5399c5e81016c5fbb573b16
Viktoria Smirnova <vssmirnova96@gmail.com>
2023-06-04 22:18:21 +0300

    parameterized

--- src/test/java/ru/netology/services/RestServiceTest.java
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -2,36 +2,19 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.RestService;
 
 public class RestServiceTest {
 
-    @Test
-    void Example1() {
+    @ParameterizedTest
+    @CsvSource({
+            "10000, 3000, 20000, 3",
+            "100000, 60000, 150000, 2"
+            })
+    void Example1(int income, int expenses, int threshold, int expected) {
         RestService service = new RestService();
-
-        // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold  = 20_000;
-        int expected = 3;
-
-        // вызываем целевой метод:
-        int actual = service.calculate(income, threshold, expenses);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void Example2() {
-        RestService service = new RestService();
-
-        // подготавливаем данные:
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold  = 150_000;
-        int expected = 2;
 
         // вызываем целевой метод:
         int actual = service.calculate(income, threshold, expenses);
